da24201f7e7313b287c0a71d4b1cf10f76b05891
ToniPortal <tonitoctoc13@gmail.com>
2021-11-26 15:20:07 +0100

    lancement

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
-
+const {
+    app,
+    BrowserWindow,
+    ipcMain,
+    nativeTheme
+} = require('electron')
 const path = require('path')
+
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 1000,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('./web/index.html')
+
+
+    ipcMain.on('audiostart', (event, arg) => {
+
+
+    });
+
+}
 
 app.whenReady().then(() => {
     createWindow()
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
     })
 })
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 500,
-        height: 800,
-        webPreferences: {
-          preload: path.join(__dirname, 'preload.js')
-        }
-      })
-
-    win.loadFile('./web/index.html')
-
-    ipcMain.handle('dark-mode:toggle', () => {
-        if (nativeTheme.shouldUseDarkColors) {
-          nativeTheme.themeSource = 'light'
-        } else {
-          nativeTheme.themeSource = 'dark'
-        }
-        return nativeTheme.shouldUseDarkColors
-      })
-    
-      ipcMain.handle('dark-mode:system', () => {
-        nativeTheme.themeSource = 'system'
-      })
-}
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
--- preload.js
+++ preload.js
@@ -1,5 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  const ipc = require('electron').ipcRenderer;
+
+  document.getElementById('audiostart').addEventListener('click', () => {
+      ipc.send('audiostart');
+  });
+
   const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
       if (element) element.innerText = text
